87bc50c207bb6fe34e4532192636873310cc3792
David <39525540+tracktor1@users.noreply.github.com>
2018-11-01 21:12:49 +0200

    Update Exchange/2013/Database-free-space.bat

---
+++ Exchange/2013/Database-free-space.bat
@@ -0,0 +1,2 @@
+C:\WINDOWS\system32\windowspowershell\v1.0\PowerShell.exe -PSConsoleFile "C:\Program Files\Microsoft\Exchange Server\V15\Bin\ExShell.psc1" -Command ".\Database-free-space.ps1"
+pause
