1e3c5c99b4f50aa9b794553dce6631bb2294ada9
David <39525540+tracktor1@users.noreply.github.com>
2019-08-30 19:47:31 +0300

    Create Reset User Password.ps1

---
+++ Active Directory/Reset User Password.ps1
@@ -0,0 +1,59 @@
+<#
+This script will check if Active Directory module is enabled then ask for the username to reset password.
+after reseting the password it will enable the user must change password at next logon checkbox.
+
+
+DA-#>
+
+# Change to $false to deisable the user must change password at next logon 
+[bool] $ChngPass = $true
+
+# Check if Active Directory module is enabled, if not try to import.
+If ( (Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue) -eq $null ) {
+    Try {
+        Import-Module ActiveDirectory -ErrorAction stop
+    } Catch {
+        # capture any failure and display it in the error section, then end the script with a return
+        # code of 1 so that CU sees that it was not successful
+		write-host '[*]   ----------------------------------------------------------------------------------------'
+		write-host '[*]   | RSAT Tools or ActiveDirectory module is not installed on this computer               |'
+		write-host '[*]   | Please run: "Add-WindowsFeature RSAT-AD-PowerShell" to install the powershell module |'
+		write-host '[*]   ----------------------------------------------------------------------------------------'
+        #Write-Error "Not able to load the Module" -ErrorAction Continue
+        #Write-Error $Error[1] -ErrorAction Continue
+        Exit 1
+    }
+}
+
+
+$User = Read-Host '[+] Username to reset password is'
+for ($i=1; $i -le 10; $i++) {
+Write-Host '****' -NoNewline
+start-sleep -milliseconds 10
+}
+write-host
+# Check if the user exist
+Try {
+	$Getuser = Get-ADUser $User -ErrorAction stop
+	$UserName = $Getuser.Name
+	write-host '[+] Please confirm, the user is' $UserName
+	$confirmation = Read-Host '[+] [Y] to confirm or any key to cancel'
+	if ($confirmation -eq 'y') {
+		write-host 'user is:' $UserName
+		$NewPassword = (Read-Host -Prompt "Provide New Password" -AsSecureString)
+		Set-Aduser $User -ChangePasswordAtLogon $ChngPass
+	}
+	Else {
+		Exit 1
+	}
+} 
+catch {
+	write-host 'User does not exist check username and try again' -ForegroundColor red
+		Exit 1
+}
+
+
+#Set-ADAccountPassword -Identity $User -NewPassword $NewPassword -Reset
+
+
+
