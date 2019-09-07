3cbb5658846cea34fabff865594e06404d4a83bb
David Azar <39525540+tracktor1@users.noreply.github.com>
2019-09-08 00:20:03 +0300

    Update Download File From Github.ps1

--- Windows/Download File From Github.ps1
+++ Windows/Download File From Github.ps1
@@ -4,7 +4,7 @@ $Fileurl = "https://raw.githubusercontent.com/tracktor1/PowerShell/master/Active
 # Destination file name
 $filetosave = ".\Reset User Password.ps1" 
 
-#Use Tls1.2 to avoid Errors
+#Force Tls1.2 to avoid Errors
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Invoke-WebRequest -Uri $Fileurl -OutFile $filetosave
