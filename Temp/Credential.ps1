4ed97d9e890c78f5fc1e42d4fb21ef4ba21e55be
David <39525540+tracktor1@users.noreply.github.com>
2019-09-21 16:18:03 +0300

    Update Credential.ps1

--- Temp/Credential.ps1
+++ Temp/Credential.ps1
@@ -1,20 +1,62 @@
-$credentials = Get-Credential
-$filename = 'secretfile.txt'
-$Filepath = 'C:\Cred-safe\'
-$credentials | Export-CliXml -Path $filename
+#$credentials = Get-Credential
+$Filename = 'secretfile.txt'
+$FolderPath = 'C:\Cred-Rep\'
+$Saveto = Join-Path $FolderPath $Filename
+#$credentials | Export-CliXml -Path $Saveto
 
-# Check if $filename is directory or file
-if ((Get-Item $filename) -is [System.IO.DirectoryInfo]) {
+# Check if path exists
+if (!(Test-Path $FolderPath)){
+	write-host "[-] Directory not found, creating..."
+	try {
+		New-Item -ItemType Directory -Force -Path $FolderPath -ErrorAction stop | Out-Null
+	}
+	catch {
+		write-host 'Script has errors, cannot creat Directory in path' -ForegroundColor red
+		Exit 1
+	}
+}
+# Check if file exists
+if ((Test-Path $Saveto -PathType leaf)){
+	Write-Host '[-] File $Filename found in repository, overwrite?'
+	$Overwrite = Read-Host '[+] Press [Y] if you want to overwrite or any key to skip'
+	if ($Overwrite -eq 'y') {
+		$credentials = Get-Credential
+		$credentials | Export-CliXml -Path $Saveto
+		write-host 'Saved credentials in $Saveto'
+	}
+	else {
+		write-host '[-] Did nothing...' -ForegroundColor Yellow
+	}
+	try {
+		$Folder = Split-Path -Path $Filename
+		New-Item -ItemType Directory -Force -Path $Folder -ErrorAction stop | Out-Null
+	}
+	catch {
+		write-host 'Script has errors, cannot creat path' -ForegroundColor red
+		Exit 1
+		}
+}
+else {
+	$credentials = Get-Credential
+	$credentials | Export-CliXml -Path $Saveto
+	write-host 'Saved credentials in $Saveto'
+}
+
+
+<#
+# Check if $Filename is directory or file
+if ((Get-Item $Filename) -is [System.IO.DirectoryInfo]) {
 	write-host = "xxxxxx"
 	}
 else {
 	write-host = "YYYYYY"
 }
+
 #Test path exists
-if (!(Test-Path $filename -PathType leaf)){
-	write-host "File $filename not found, creating..."
+if (!(Test-Path $Filename -PathType leaf)){
+	write-host "File $Filename not found, creating..."
 	try {
-		$Folder = Split-Path -Path $filename
+		$Folder = Split-Path -Path $Filename
 		New-Item -ItemType Directory -Force -Path $Folder -ErrorAction stop | Out-Null
 		}
 	catch {
@@ -22,3 +64,16 @@ if (!(Test-Path $filename -PathType leaf)){
 		Exit 1
 		}
 	}
+if (!(Test-Path $Filepath -PathType leaf)){
+	write-host "File $Filename not found, creating..."
+	try {
+		$Folder = Split-Path -Path $Filename
+		New-Item -ItemType Directory -Force -Path $Folder -ErrorAction stop | Out-Null
+		}
+	catch {
+		write-host 'Script has errors, cannot creat path' -ForegroundColor red
+		Exit 1
+		}
+	}
+	
+#>
