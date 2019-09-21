a676578104761286a7e108aaff73e2a54a5d6119
David <39525540+tracktor1@users.noreply.github.com>
2019-09-21 16:36:45 +0300

    cleanup

--- Temp/Credential.ps1
+++ Temp/Credential.ps1
@@ -1,8 +1,13 @@
-#$credentials = Get-Credential
+<#
+Save credentials in file repository
+
+DA-#>
+
+
 $Filename = 'secretfile.txt'
 $FolderPath = 'C:\Cred-Rep\'
 $Saveto = Join-Path $FolderPath $Filename
-#$credentials | Export-CliXml -Path $Saveto
+
 
 # Check if path exists
 if (!(Test-Path $FolderPath)){
@@ -34,39 +39,3 @@ else {
 	$credentials | Export-CliXml -Path $Saveto
 	write-host "[+] Saved credentials in $Saveto" -ForegroundColor Green
 }
-
-
-<#
-# Check if $Filename is directory or file
-if ((Get-Item $Filename) -is [System.IO.DirectoryInfo]) {
-	write-host = "xxxxxx"
-	}
-else {
-	write-host = "YYYYYY"
-}
-
-#Test path exists
-if (!(Test-Path $Filename -PathType leaf)){
-	write-host "File $Filename not found, creating..."
-	try {
-		$Folder = Split-Path -Path $Filename
-		New-Item -ItemType Directory -Force -Path $Folder -ErrorAction stop | Out-Null
-		}
-	catch {
-		write-host 'Script has errors, cannot creat path' -ForegroundColor red
-		Exit 1
-		}
-	}
-if (!(Test-Path $Filepath -PathType leaf)){
-	write-host "File $Filename not found, creating..."
-	try {
-		$Folder = Split-Path -Path $Filename
-		New-Item -ItemType Directory -Force -Path $Folder -ErrorAction stop | Out-Null
-		}
-	catch {
-		write-host 'Script has errors, cannot creat path' -ForegroundColor red
-		Exit 1
-		}
-	}
-	
-#>
