2258a4958926a132a46572f6fac458f35e6c10bb
David <39525540+tracktor1@users.noreply.github.com>
2019-09-26 07:12:02 +0300

    Test menu

---
+++ Temp/csv-menu.ps1
@@ -0,0 +1,20 @@
+$csv = Import-CSV -Path .\csv-menu.csv
+
+
+$menu = @{}
+
+foreach($Line in $csv) {
+	$menuorder = $Line.order
+	$menudesc = $Line.description
+	$menudo = $Line.do
+	{ Write-Host "$menuorder. $($processes[$i-1].name)"
+	$menu.Add($i,($processes[$i-1].name)) }
+
+[int]$ans = Read-Host 'Enter selection'
+$selection = $menu.Item($ans) ; Get-Process $selection
+
+
+for ($i=1;$i -le $customerlist.count; $i++) {
+    Write-Host "$i. $($customerlist[$i-1].name)"
+    $menu.Add($i,($customerlist[$i-1].name))
+    }
