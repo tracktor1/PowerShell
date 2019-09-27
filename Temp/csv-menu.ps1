2ce2e057ff6b0b1d9c8d0e240683da9fee5797f2
David <39525540+tracktor1@users.noreply.github.com>
2019-09-27 13:07:00 +0300

    Update csv-menu.ps1

--- Temp/csv-menu.ps1
+++ Temp/csv-menu.ps1
@@ -7,9 +7,10 @@ foreach($Line in $csv) {
 	$menuorder = $Line.order
 	$menudesc = $Line.description
 	$menudo = $Line.do
-	{ Write-Host "$menuorder. $($processes[$i-1].name)"
-	$menu.Add($i,($processes[$i-1].name)) }
-
+	Write-Host "$menuorder. $($processes[$i-1].name)"
+	#$menu.Add($i,($processes[$i-1].name)) 
+	}
+<#
 [int]$ans = Read-Host 'Enter selection'
 $selection = $menu.Item($ans) ; Get-Process $selection
 
@@ -18,3 +19,4 @@ for ($i=1;$i -le $customerlist.count; $i++) {
     Write-Host "$i. $($customerlist[$i-1].name)"
     $menu.Add($i,($customerlist[$i-1].name))
     }
+#>
