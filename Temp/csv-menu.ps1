e0ba96c34cb14aa9eaf6eea6aaa407bae4101121
David <39525540+tracktor1@users.noreply.github.com>
2019-09-27 17:37:39 +0300

    Update csv-menu.ps1

--- Temp/csv-menu.ps1
+++ Temp/csv-menu.ps1
@@ -5,9 +5,10 @@ foreach($Line in $csv) {
 	$menuorder = $Line.order
 	$menudesc = $Line.description
 	$menudo = $Line.do
-	Write-Host $menudesc
+	Write-Host "[+] $menuorder - $menudesc"
 }
 
-[int]$ans = Read-Host 'Please select menu item to run'
-$selection = $menu.Item($ans) ; Get-Process $selection
-
+[int]$ans = Read-Host 'Please select menu item to run 1,2,3...'
+$selection = $csv[$ans-1]
+write-host $selection
+$selection.do
