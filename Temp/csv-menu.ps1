6eae470702106122ebc015d0a4cea88190137f0d
David <39525540+tracktor1@users.noreply.github.com>
2019-09-27 13:38:14 +0300

    Update csv-menu.ps1

--- Temp/csv-menu.ps1
+++ Temp/csv-menu.ps1
@@ -1,23 +1,13 @@
 $csv = Import-CSV -Path .\csv-menu.csv
 
 
-$menu = @{}
-
 foreach($Line in $csv) {
 	$menuorder = $Line.order
 	$menudesc = $Line.description
 	$menudo = $Line.do
 	Write-Host $menudesc
-	#Write-Host "$menuorder. $($processes[$i-1].name)"
-	#$menu.Add($i,($processes[$i-1].name)) 
-	}
-<#
-[int]$ans = Read-Host 'Enter selection'
+}
+
+[int]$ans = Read-Host 'Please select menu item to run'
 $selection = $menu.Item($ans) ; Get-Process $selection
 
-
-for ($i=1;$i -le $customerlist.count; $i++) {
-    Write-Host "$i. $($customerlist[$i-1].name)"
-    $menu.Add($i,($customerlist[$i-1].name))
-    }
-#>
