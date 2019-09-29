a4da67d707ffaa8e4a7bd59f8bda5e5b4117b463
David <39525540+tracktor1@users.noreply.github.com>
2019-09-29 12:59:47 +0300

    Update csv-menu.ps1

--- Temp/csv-menu.ps1
+++ Temp/csv-menu.ps1
@@ -9,6 +9,22 @@ foreach($Line in $csv) {
 }
 
 $ans = Read-Host 'Please select menu item to run 1,2,3...'
+try {
+	if($ans -eq "Q") {
+	write-host Bay Bay
+	break
+	}
+	$test = [int]$ans
+	if ($ans -gt ($csv.count)) {
+	write-host Bay Bay
+	break
+	}
+}
+catch {
+	write-host Bay Bay
+	break
+}
+
 foreach($Line in $csv) {
 	if ($Line.order -eq $ans){
 	write-host $Line.do
