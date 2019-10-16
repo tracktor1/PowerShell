2491189cb43fe859161390d705f78cbc4ac56f4f
David <39525540+tracktor1@users.noreply.github.com>
2019-10-16 07:58:55 +0300

    Update Delay-Bar.ps1

--- Temp/Delay-Bar.ps1
+++ Temp/Delay-Bar.ps1
@@ -1,14 +1,37 @@
-$I = 1
-$time = 10
-While($I -lt $time) {
-	$P = $I/$time*100
-	Write-Progress -Activity "Delay the next VM startup" -Status "$I% Complete:" -PercentComplete $P;
-	start-sleep 1
-	$I++
-	if ($host.UI.RawUI.KeyAvailable) {
-		$key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp,IncludeKeyDown")
-			if ($key.KeyDown -eq "True"){
-				break    
-			}    
+<#
+	.NAME
+		Delay-Bar
+	
+	.SYNOPSIS
+		Delay-Bar - Display a progress bar counting seconds
+	
+	.DESCRIPTION 
+		This function displays a progress bar counting seconds
+
+	.PARAMETER Seconds
+		seconds to count
+	
+    .EXAMPLE
+		Delay-Bar -Seconds 10
+DA-#>
+
+Function Delay-Bar {
+	Param (
+	[Parameter(Mandatory=$true)] [int]$time
+	)
+	$I = 1
+		While($I -lt $time) {
+		$P = $I/$time*100
+		Write-Progress -Activity "Delay the next VM startup" -Status "$I% Complete:" -PercentComplete $P;
+		start-sleep 1
+		$I++
+		if ($host.UI.RawUI.KeyAvailable) {
+			$key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp,IncludeKeyDown")
+				if ($key.KeyDown -eq "True"){
+					break    
+				}    
+		}
 	}
 }
+
+Delay-Bar 10
