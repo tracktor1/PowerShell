73850bb00acad76ed8b74af9e72c3c66586e961c
David <39525540+tracktor1@users.noreply.github.com>
2019-10-26 23:51:19 +0300

    Create AD-import-csv.ps1

---
+++ Temp/AD-import-csv.ps1
@@ -0,0 +1,37 @@
+<#
+
+This script requires a csv file (users.csv) in the same folder
+The file must contain these column names in order to work:
+FirstName,LastName,Pass,OuPath,UserMail
+
+DA-#>
+
+Import-Module activedirectory
+
+# CSV file name and path
+$ADUserList = Import-csv .\users.csv
+#Active directory distinguished name
+$DomainDN = "DC=Domain,DC=local"
+#Active directory domain name
+$ADdomain = "Domain.local"
+
+foreach ($User in $ADUserList) {
+	$Firstname = $User.FirstName
+	$Lastname = $User.LastName
+	$Password = $User.Pass
+	$OU = $User.OuPath
+	$Email = $User.UserMail
+	
+	$usersplit = $Email.split("@")[0]	
+	
+	#Check to see if the user already exists in AD
+	if (Get-ADUser -F {SamAccountName -eq $usersplit}) {
+		 #If user exists, give a warning
+		 Write-Warning "A user account with username $usersplit already exist in Active Directory."
+	}
+	else {
+	$JoinOU = "OU=$OU,$DomainDN"
+	$PrincipalName = $usersplit + "@" + $ADdomain
+	New-ADUser -SamAccountName $usersplit -UserPrincipalName $PrincipalName -EmailAddress "$Email" -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -Enabled $True -DisplayName "$Firstname $Lastname" -Path $JoinOU -AccountPassword (convertto-securestring $Password -AsPlainText -Force) 
+	}
+}
