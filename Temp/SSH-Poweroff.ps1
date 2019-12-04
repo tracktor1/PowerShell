16f5a0beedb21ae97ba2cace0580830771084e71
David <39525540+tracktor1@users.noreply.github.com>
2019-12-04 10:19:14 +0200

    Update SSH-Poweroff.ps1

--- Temp/SSH-Poweroff.ps1
+++ Temp/SSH-Poweroff.ps1
@@ -14,19 +14,22 @@ for ($i=0; $i -le 10; $i++) {
 	Remove-SSHSession -SessionId $i
 }
 
-$session = New-SSHSession -ComputerName $IP -Credential $cred -AcceptKey
-Get-SSHSession
-$sessionstream = New-SSHShellStream -SessionId $session.SessionId
-$sessionstream.read() #| out-null
-$user = Invoke-SSHCommand -Sessionid $sessionstream.SessionId -Command "whoami"
-$string = "[sudo] password for $($user.output):"
-Invoke-SSHStreamExpectSecureAction -Command 'sudo -i' -ShellStream $sessionstream -ExpectString $string -SecureAction $cred.password
-$sessionstream.read()
-Start-Sleep -Seconds 1
-Invoke-SSHStreamShellCommand -ShellStream $sessionstream -Command $runcmd
-Start-Sleep -Seconds 5
-$sessionstream.read()
-Remove-SSHSession -SessionId $session.SessionId
-
-
+if (Test-Connection -ComputerName $IP -Count 1 -Quiet) {
+	$session = New-SSHSession -ComputerName $IP -Credential $cred -AcceptKey
+	Get-SSHSession
+	$sessionstream = New-SSHShellStream -SessionId $session.SessionId
+	$sessionstream.read() #| out-null
+	$user = Invoke-SSHCommand -Sessionid $sessionstream.SessionId -Command "whoami"
+	$string = "[sudo] password for $($user.output):"
+	Invoke-SSHStreamExpectSecureAction -Command 'sudo -i' -ShellStream $sessionstream -ExpectString $string -SecureAction $cred.password
+	$sessionstream.read()
+	Start-Sleep -Seconds 1
+	Invoke-SSHStreamShellCommand -ShellStream $sessionstream -Command $runcmd
+	Start-Sleep -Seconds 5
+	$sessionstream.read()
+	Remove-SSHSession -SessionId $session.SessionId
+}
+else {
+	Write-Warning "No Ping to host $IP skipping..."
+}
 
