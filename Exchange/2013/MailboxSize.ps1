45b19aa7f6bd5ed57054f68640d21f576e74ae0f
David <39525540+tracktor1@users.noreply.github.com>
2019-12-14 23:47:51 +0200

    Update MailboxSize.ps1

--- Exchange/2013/MailboxSize.ps1
+++ Exchange/2013/MailboxSize.ps1
@@ -1,10 +1,25 @@
 
 
-
 $tt= Get-Date -UFormat "%Y-%m-%d"
-Get-Mailbox | Get-MailboxStatistics | Sort totalitemsize -desc | ft displayname, totalitemsize, itemcount > c:\$tt-mailboxes.txt
+$FolderPath = 'C:\MailboxSize\'
+$EXdb = Get-MailboxDatabase
+
+# Check if path exists
+if (!(Test-Path $FolderPath)){
+	write-host "[-] Directory not found, creating..."
+	try {
+		New-Item -ItemType Directory -Force -Path $FolderPath -ErrorAction stop | Out-Null
+	}
+	catch {
+		write-host 'Script has errors, cannot creat Directory in path' -ForegroundColor red
+		Exit 1
+	}
+}
+
+foreach ($db in $EXdb) {
+	$Filename = $tt + '-' + $db.name + '.txt'
+	$Saveto = Join-Path $FolderPath $Filename
+	Get-Mailbox -Database $db.name | Get-MailboxStatistics | Sort totalitemsize -desc | ft displayname, totalitemsize, itemcount > $Saveto
+
+}
 exit
-
-
-
-
